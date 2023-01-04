339f0247340a2027116637c2da5c65c64dc01c1d
egboyguda <gboyguda@gmail.com>
2023-01-04 20:30:00 +0800

    get client id

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ const path = require('path');
 const PORT = 3000 
 const ejsMate = require('ejs-mate');
 const partials = require('express-partials');
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: true});
 const { exec } = require('child_process');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.engine("ejs",ejsMate)
-
+app.set('trust proxy', true)
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.get("/", (req, res) => {
-    const clientip = req.query.clientip
+app.use(require('body-parser').urlencoded({ extended: true }))
+app.get("/", urlencodedParser,(req, res) => {
+    const clientip = req.ip
     console.log(clientip)
     res.render("portal")
 })
