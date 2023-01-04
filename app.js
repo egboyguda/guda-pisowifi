c6ddbf614b7fdef854b41f496ca5c4a96ec6f447
egboyguda <gboyguda@gmail.com>
2023-01-04 15:53:56 +0800

    get ip from requisting client

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const path = require('path');
 const PORT = 3000 
 const ejsMate = require('ejs-mate');
 const partials = require('express-partials');
-
-
+const { exec } = require('child_process');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.engine("ejs",ejsMate)
@@ -13,6 +12,8 @@ app.engine("ejs",ejsMate)
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.get("/", (req, res) => {
+    const clientip = req.query.clientip
+    console.log(clientip)
     res.render("portal")
 })
 
